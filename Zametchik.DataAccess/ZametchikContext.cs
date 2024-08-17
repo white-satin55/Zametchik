d5c0c6805d746b3949d88c13b35f8ffaae28be32
eric <ericmaximov1@gmail.com>
2024-08-17 13:13:23 +0300

    обновлен класс контекста

---
+++ Zametchik.DataAccess/ZametchikContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Zametchik.Domain;
+
+namespace Zametchik.DataAccess;
+
+public class ZametchikContext(DbContextOptions<ZametchikContext> options) : DbContext(options)
+{
+    public DbSet<ToDoTaskEntity> ToDoTasks => Set<ToDoTaskEntity>();
+    public DbSet<UserEntity> Users => Set<UserEntity>();
+    public DbSet<TagEntity> Tags => Set<TagEntity>();
+    public DbSet<UserStatisticEntity> UserStatistics => Set<UserStatisticEntity>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+
+}
