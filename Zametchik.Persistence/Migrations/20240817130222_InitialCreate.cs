cafc723ea8cec057074930da326fa0b86cc777c3
eric <ericmaximov1@gmail.com>
2024-08-17 17:18:20 +0300

    добавлены миграции

---
+++ Zametchik.Persistence/Migrations/20240817130222_InitialCreate.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Zametchik.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserStatisticGuid = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tags",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    CompletedToDoTasksCount = table.Column<int>(type: "integer", nullable: false),
+                    ExpiredToDoTasksCount = table.Column<int>(type: "integer", nullable: false),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tags", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Tags_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ToDoTasks",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreationDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Deadline = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    AcceptableDelay = table.Column<TimeSpan>(type: "interval", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false),
+                    Priority = table.Column<int>(type: "integer", nullable: false),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ToDoTasks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ToDoTasks_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserStatistics",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    AccountAge = table.Column<TimeSpan>(type: "interval", nullable: false),
+                    CompletedToDoTasksCount = table.Column<int>(type: "integer", nullable: false),
+                    ExpiredToDoTasksCount = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserStatistics", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserStatistics_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TagEntityToDoTaskEntity",
+                columns: table => new
+                {
+                    TagsId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ToDoTasksId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TagEntityToDoTaskEntity", x => new { x.TagsId, x.ToDoTasksId });
+                    table.ForeignKey(
+                        name: "FK_TagEntityToDoTaskEntity_Tags_TagsId",
+                        column: x => x.TagsId,
+                        principalTable: "Tags",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TagEntityToDoTaskEntity_ToDoTasks_ToDoTasksId",
+                        column: x => x.ToDoTasksId,
+                        principalTable: "ToDoTasks",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TagEntityToDoTaskEntity_ToDoTasksId",
+                table: "TagEntityToDoTaskEntity",
+                column: "ToDoTasksId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tags_UserId",
+                table: "Tags",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ToDoTasks_UserId",
+                table: "ToDoTasks",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserStatistics_UserId",
+                table: "UserStatistics",
+                column: "UserId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TagEntityToDoTaskEntity");
+
+            migrationBuilder.DropTable(
+                name: "UserStatistics");
+
+            migrationBuilder.DropTable(
+                name: "Tags");
+
+            migrationBuilder.DropTable(
+                name: "ToDoTasks");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ Zametchik.Persistence/ZametchikContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Zametchik.Domain;
+
+namespace Zametchik.DataAccess;
+
+public class ZametchikContext(DbContextOptions<ZametchikContext> options) : DbContext(options)
+{
+    public DbSet<ToDoTaskEntity> ToDoTasks => Set<ToDoTaskEntity>();
+    public DbSet<UserEntity> Users => Set<UserEntity>();
+    public DbSet<TagEntity> Tags => Set<TagEntity>();
+    public DbSet<UserStatisticEntity> UserStatistics => Set<UserStatisticEntity>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
