7d0cdfc8e4b818783d9849c7abff04acca25d2d3
eric <ericmaximov1@gmail.com>
2024-08-18 17:58:01 +0300

    some fixes

--- Zametchik.Application/CQRS/Commands/ToDoTasks/CreateToDoTaskCommand/CreateToDoTaskCommandValidator.cs
+++ Zametchik.Application/CQRS/Commands/ToDoTasks/CreateToDoTaskCommand/CreateToDoTaskCommandValidator.cs
@@ -4,8 +4,5 @@ using FluentValidation.Results;
 namespace Zametchik.Application.CQRS.Commands.ToDoTasks.CreateToDoTaskCommand;
 internal class CreateToDoTaskCommandValidator : AbstractValidator<CreateToDoTaskCommand>
 {
-    public override ValidationResult Validate(ValidationContext<CreateToDoTaskCommand> context)
-    {
-        return base.Validate(context);
-    }
+    
 }
