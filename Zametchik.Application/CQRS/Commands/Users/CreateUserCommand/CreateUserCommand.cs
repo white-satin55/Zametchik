62850627524a7b0e3a6f9b7bb054e9be3329612f
eric <ericmaximov1@gmail.com>
2024-08-19 16:45:11 +0300

    рабочая команда для создания пользователя

---
+++ Zametchik.Application/CQRS/Commands/Users/CreateUserCommand/CreateUserCommand.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+
+namespace Zametchik.Application.CQRS.Commands.Users.CreateUserCommand;
+public class CreateUserCommand : IRequest<Guid>
+{
+    public string UserName { get; set; }
+}
---
+++ Zametchik.Application/CQRS/Commands/Users/CreateUserCommand/CreateUserCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿
+using MediatR;
+
+using Zametchik.Application.Repositories;
+using Zametchik.Domain;
+
+namespace Zametchik.Application.CQRS.Commands.Users.CreateUserCommand;
+internal class CreateUserCommandHandler : IRequestHandler<CreateUserCommand, Guid>
+{
+    private readonly IAsyncRepository<UserEntity> _userRepository;
+
+    public CreateUserCommandHandler(IAsyncRepository<UserEntity> userRepository)
+    {
+        _userRepository = userRepository;
+    }
+
+    public async Task<Guid> Handle(CreateUserCommand request, CancellationToken cancellationToken)
+    {
+        var validator = new CreateUserCommandValidator();
+        var validationResult = validator.Validate(request);
+
+        var user = new UserEntity()
+        {
+            UserName = request.UserName,
+        };
+
+        return await _userRepository.AddAsync(user);
+    }
+}
