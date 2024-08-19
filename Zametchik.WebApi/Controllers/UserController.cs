1ba9438f3cf426b141ee2ee337b9c177482186b6
eric <ericmaximov1@gmail.com>
2024-08-19 16:51:58 +0300

    создан контроллер для пользователей

---
+++ Zametchik.WebApi/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+
+using Microsoft.AspNetCore.Mvc;
+
+using Zametchik.Application.CQRS.Commands.Users.CreateUserCommand;
+
+namespace Zametchik.WebApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UserController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public UserController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost]
+    [Route("[action]")]
+    public async Task<ActionResult<Guid>> User(CreateUserCommand command)
+    {
+        var result = await _mediator.Send(command);
+
+        return Ok(result);
+    }
+}
