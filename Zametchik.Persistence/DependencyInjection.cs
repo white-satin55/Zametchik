56e725b75e46384da1fb0ecd71737355d7d387b5
eric <ericmaximov1@gmail.com>
2024-08-19 16:49:26 +0300

    добавлен Persistence DI

---
+++ Zametchik.Persistence/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+using Zametchik.Application.Repositories;
+using Zametchik.Domain;
+using Zametchik.Persistence.Repositories.AsyncRepositories;
+
+namespace Zametchik.Persistence;
+public static class DependencyInjection
+{
+    public static IServiceCollection AddPersistence(this IServiceCollection services)
+    {        
+        services.AddScoped<IAsyncReadonlyRepository<ToDoTaskEntity>, ToDoTaskAsyncRepository>();
+        services.AddScoped<IAsyncRepository<ToDoTaskEntity>, ToDoTaskAsyncRepository>();        
+        services.AddScoped<IAsyncReadonlyRepository<UserEntity>, UserAsyncRepository>();
+        services.AddScoped<IAsyncRepository<UserEntity>, UserAsyncRepository>();        
+
+        return services;
+    }
+}
