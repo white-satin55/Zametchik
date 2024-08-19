84b873f2fc523230c2db164be6eae98c0dd74be0
eric <ericmaximov1@gmail.com>
2024-08-19 16:47:34 +0300

    добавлен Application DI

---
+++ Zametchik.Application/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Zametchik.Application;
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+        services.AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+        });       
+
+        return services;
+    }
+}
