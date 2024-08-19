c0ebb2167dfd1ce7e2a25fdbfbc2941947726b7e
eric <ericmaximov1@gmail.com>
2024-08-19 16:52:39 +0300

    класс Program в сборке WebApi

---
+++ Zametchik.WebApi/Program.cs
@@ -0,0 +1,38 @@
+using Microsoft.EntityFrameworkCore;
+using Zametchik.Persistence;
+using Zametchik.Application;
+
+var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ZametchikContext>(options =>
+{
+    options.UseNpgsql(configuration.GetConnectionString(nameof(ZametchikContext)));
+});
+
+builder.Services.AddPersistence();
+builder.Services.AddApplication();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
